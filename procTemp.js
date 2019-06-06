a6e145d4029c904e16726e207f3278b65702e15d
Artyom D. Alekseev <rexolion@localhost.localdomain>
2019-06-06 20:11:07 +0300

    :star: feat: add johnny-five and argoritms

--- procTemp.js
+++ procTemp.js
@@ -1,6 +1,17 @@
 const rl = require('readline');
 const fs = require('fs');
+const five = require('johnny-five');
+const board = new five.Board();
+let currTemp;
 
+board.on('ready', function() {
+  const lcd = five.LCD({
+    pins: [7,8,9,10,11,12],
+    backlight: 6,
+    rows: 2,
+    cols: 20,
+  });
+  
 function readfile() {
   let rlInterface = rl.createInterface({
     input: fs.createReadStream('./currTemp'),
@@ -8,8 +19,11 @@ function readfile() {
     console: false,
   });
 
-  rlInterface.on('line', line => console.log(line));
+rlInterface.on('line', line => { lcd.clear().print(line) }) 
 }
 
 fs.watchFile('./currTemp', readfile);
+
+  
+})
 
